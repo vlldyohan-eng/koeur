a93e675f498480ee623dac144780206d259d3bcd
vlldyohan-eng <vlld.yohan@gmail.com>
2026-08-18 07:24:04 +0200

    Implement WebView to load local HTML file

--- app/src/main/java/com/koeur/app/MainActivity.java
+++ app/src/main/java/com/koeur/app/MainActivity.java
@@ -1,1 +1,25 @@
+package com.koeur.app;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient());
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+
+        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(webView);
+    }
+}
